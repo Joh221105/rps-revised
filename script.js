d1d968aaa0a1250ad0aa41edbcee8382117860f9
John <codingpractice105@gmail.com>
2022-11-14 14:06:35 -0500

    completed the game, not very flashy

--- script.js
+++ script.js
@@ -1,35 +1,65 @@
 const CHOICES = ["rock", "paper", "scissors"]
 
+const playerPoints = document.querySelector('#pscore');
+const computerPoints = document.querySelector('#cscore');
+
+let cscore = 0;
+let pscore = 0;
+
+let butts = document.querySelectorAll('button');
+for (x = 0; x < butts.length; x++) {
+    butts[x].addEventListener('click', function(event){
+    let player = (event.target.innerHTML);
+    playRound(player.toLowerCase(),getComputerChoice(CHOICES));
+    })}
+
+
 function getComputerChoice(CHOICES){
     let random = Math.floor(Math.random()*CHOICES.length) 
     return CHOICES[random]
 }
 
-function getPlayerChoice() {
-    let choice = prompt("Choose: rock, paper, scissors").toLowerCase()
-    return choice
-}
 
+function playRound(player,computer) {
 
-function playRound() {
-    let computer = getComputerChoice(CHOICES);
-    let player = getPlayerChoice();
     if (computer === "scissors" && player === "paper" || computer === "rock" && player === "scissors" || computer === "paper" && player === "rock") {
-        console.log(`You lose! ${computer.toUpperCase()} beats ${player.toUpperCase()}.`)
+
+        document.querySelector("#message").innerHTML = `You lose! ${computer.toUpperCase()} beats ${player.toUpperCase()}.`
+
+        computerPoints.textContent = ++cscore;
+
+        document.querySelector("#cscore").innerHTML = `${cscore}`
+
+
+
     } else if (computer === player){
-        console.log("It's a tie!")
+
+        document.querySelector("#message").innerHTML = "It's a tie!"
+
     } else if(computer === "paper" && player === "scissors" || computer === "scissors" && player === "rock" || computer === "rock" && player === "paper"){
-        console.log(`You win! ${player.toUpperCase()} beats ${computer.toUpperCase()}.`)
+
+        document.querySelector("#message").innerHTML = `You win! ${player.toUpperCase()} beats ${computer.toUpperCase()}.`;
+
+        playerPoints.textContent = ++pscore;
+
+        document.querySelector("#pscore").innerHTML = `${pscore}`
+
+    }
+    checkWinner();
+
+}
+
+function checkWinner() {
+    if (pscore ===5) {
+        document.querySelector("#message").innerHTML = `You WIN the game! Congratulations!`
+        toggler()
+    } else if (cscore ===5) {
+        document.querySelector("#message").innerHTML = `You LOSE, BOO YOU SUCK!`
+        toggler()
     }
 
 }
 
-function game() {
-    let rounds = 0;
-    while (rounds < 5) {
-        playRound();
-        rounds ++;
+function toggler(){
+    document.querySelector("#body").classList.toggle("gameover");
     }
-}
-
-game()
